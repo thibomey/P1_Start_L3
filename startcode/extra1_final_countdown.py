03d01fb20b09091d5a33510146977f68e6b6fab3
TessaCodefever <157284138+TessaCodefever@users.noreply.github.com>
2024-01-21 13:37:24 +0100

    Add while requirement and improve spacing for snippets
    Increasing the spacing improves legibility.

--- startcode/extra1_final_countdown.py
+++ startcode/extra1_final_countdown.py
@@ -1,3 +1,3 @@
-# Schrijf een programma dat:
+# Schrijf een programma dat met een while-lus:
 # - de gebruiker vraagt om een positief geheel getal in te voeren
 # - een countdown uitvoert vanaf het gekozen getal tot 0
--- startcode/snippets.py
+++ startcode/snippets.py
@@ -1,5 +1,6 @@
 import turtle
 
+
 # selectie
 if getal > 0:
     print("Het ingevoerde getal is positief.")
@@ -8,15 +9,18 @@ elif getal < 0:
 else:
     print("Het ingevoerde getal is nul.")
 
+
 # conditionele lus
 beurt_nummer = 0
 while beurt_nummer <= 5:
     print(beurt_nummer)
     beurt_nummer += 1
 
+
 # oneindige lus
 while True:
 	doe_iets()
+
 
 # for-loop
 for aantalBeurten in range(0, 5):
