f19d605446597d664bdf607806a7621b66fa5575
ThiboMeyers <149253754+ThiboMeyers@users.noreply.github.com>
2024-10-05 10:41:28 +0200

    oefeningen gemaakt

--- startcode/d_shapedrawer.py
+++ startcode/d_shapedrawer.py
@@ -4,7 +4,18 @@
 # Als de gebruiker een ongeldige vorm invoert, moet het programma een foutmelding weergeven.
 
 import turtle
+
+size = int(input("hoeveel hoeken: "))
+for i in range(size):
+    turtle.right(180-180/size)
+    turtle.forward(100)
+turtle.forward(50)
+turtle.done()
+
+
+
 input_vorm = input("Welke vorm wil je tekenen? ")
+
 if input_vorm == "vierkant":
     turtle.shape("square")
 elif input_vorm == "cirkel":
@@ -13,7 +24,25 @@ elif input_vorm == "rechthoek":
     turtle.shape("rectangle")
 elif input_vorm == "driehoek":
     turtle.shape("triangle")
+elif input_vorm == "ster":
+    for _ in range(5):
+        turtle.forward(100)
+        turtle.right(144)
+elif input_vorm == "ster met 6":
+    for _ in range(6):
+        turtle.forward(100)
+        turtle.right(60)
+        turtle.forward(100)
+        turtle.left(120)
+
 else:
     print ("Geen geldige figuur")
+
+#size = int(input("hoeveel hoeken: "))
+#for i in range(size):
+    #turtle.right(180-180/size)
+    #turtle.forward(100)
+#turtle.forward(50)
+#turtle.done()
 
 turtle.done ()
